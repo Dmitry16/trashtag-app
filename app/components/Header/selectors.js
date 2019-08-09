39f2e8c6755d2c0e4c4acefd54fb718d76e47e2c
Dmitry16 <europartner@inbox.ru>
2019-08-09 13:07:07 +0300

    head_selectors_bug

--- app/components/Header/selectors.js
+++ app/components/Header/selectors.js
@@ -4,19 +4,24 @@
 
 import { createSelector } from 'reselect';
 
-const selectHeader = (state) => state ? state.get('header') : null;
+
+const selectHeader = (state) => { 
+  // console.log('Header selectors::', state.get('header'))
+  return state.get('header') };
 
 const makeSelectLocale = () => createSelector(
   selectHeader,
-  (headerState) => headerState ? headerState.locale : null
+  (headerState) => {
+    // console.log('headerState:::', headerState)
+    return headerState.locale.get('locale')}
 );
 const makeSelectSearch = () => createSelector(
   selectHeader,
-  (headerState) => headerState ? headerState.search : null
+  (headerState) => headerState.search.get('search')
 );
 const makeSelectMenu = () => createSelector(
   selectHeader,
-  (headerState) => headerState ? headerState.menu : null
+  (headerState) => headerState.menu.get('menu')
 );
 
 export {
